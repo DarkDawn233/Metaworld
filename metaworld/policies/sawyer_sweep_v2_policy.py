ecbcbda81b5e62a570c5cc11ca68d337c4c452e1
DarkDawn233 <hjm0224@qq.com>
2023-03-01 23:18:29 +0800

    sweep

--- metaworld/policies/sawyer_sweep_v2_policy.py
+++ metaworld/policies/sawyer_sweep_v2_policy.py
@@ -6,6 +6,14 @@ from metaworld.policies.policy import Policy, assert_fully_parsed, move
 
 class SawyerSweepV2Policy(Policy):
 
+    def __init__(self):
+        self.grab_done = False
+        self.t = 0
+    
+    def reset(self):
+        self.grab_done = False
+        self.t = 0
+
     @staticmethod
     @assert_fully_parsed
     def _parse_obs(obs):
@@ -25,34 +33,43 @@ class SawyerSweepV2Policy(Policy):
             'grab_effort': 3
         })
 
-        action['delta_pos'] = move(o_d['hand_pos'], to_xyz=self._desired_pos(o_d), p=25.)
-        action['grab_effort'] = self._grab_effort(o_d)
+        action['delta_pos'] = move(o_d['hand_pos'], to_xyz=self._desired_pos(o_d, self.grab_done, self.t), p=25.)
+        action['grab_effort'], self.grab_done = self._grab_effort(o_d, self.grab_done)
+        self.t += self.grab_done
 
         return action.array
 
     @staticmethod
-    def _desired_pos(o_d):
+    def _desired_pos(o_d, grab_done, t):
         pos_curr = o_d['hand_pos']
         pos_cube = o_d['cube_pos'] + np.array([.0, .0, .015])
         pos_goal = o_d['goal_pos']
+
+        if grab_done and t < 20:
+            return pos_curr
+        elif grab_done:
+            return [pos_goal[0] - pos_cube[0] + pos_curr[0], pos_goal[1] - pos_cube[1] + pos_curr[1], pos_curr[2]]
 
         if pos_curr[0] < .2:
             if np.linalg.norm(pos_curr[:2] - pos_cube[:2]) > 0.04:
                 return pos_cube + np.array([0., 0., 0.3])
             elif abs(pos_curr[2] - pos_cube[2]) > 0.04:
                 return pos_cube
-
-        return pos_goal + np.array([0, 0, 0.1])
+        return [pos_goal[0] - pos_cube[0] + pos_curr[0], pos_goal[1] - pos_cube[1] + pos_curr[1], pos_curr[2]]
 
     @staticmethod
-    def _grab_effort(o_d):
+    def _grab_effort(o_d, grab_done):
         pos_curr = o_d['hand_pos']
-        pos_cube = o_d['cube_pos']
+        pos_cube = o_d['cube_pos'] + np.array([.0, .0, .015])
 
+        if grab_done:
+            return .7, True
         if np.linalg.norm(pos_curr[:2] - pos_cube[:2]) > 0.04 \
-            or abs(pos_curr[2] - pos_cube[2]) > 0.15:
-            return -1.
-        elif pos_cube[0] < .4:
-            return .7
+            or abs(pos_curr[2] - pos_cube[2]) > 0.045:
+            return -1., False
         else:
-            return -1.
+            return .7, True
+        # elif pos_cube[0] < .4:
+        #     return .7
+        # else:
+        #     return -1.
