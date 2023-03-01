fd6f1534c879a214fca7568976b2e6ca68ce73d0
DarkDawn233 <hjm0224@qq.com>
2023-03-01 12:15:23 +0800

    plate-slide-back

--- metaworld/policies/sawyer_plate_slide_back_v2_policy.py
+++ metaworld/policies/sawyer_plate_slide_back_v2_policy.py
@@ -13,7 +13,8 @@ class SawyerPlateSlideBackV2Policy(Policy):
             'hand_pos': obs[:3],
             'unused_1': obs[3],
             'puck_pos': obs[4:7],
-            'unused_2': obs[7:],
+            'unused_2': obs[7:-3],
+            'goal_pos': obs[-3:],
         }
 
     def get_action(self, obs):
@@ -32,15 +33,18 @@ class SawyerPlateSlideBackV2Policy(Policy):
     @staticmethod
     def _desired_pos(o_d):
         pos_curr = o_d['hand_pos']
-        pos_puck = o_d['puck_pos'] + np.array([.0, -.065, .025])
+        pos_puck_ = o_d['puck_pos'] + np.array([.0, -.065, .025])
+        pos_puck = o_d['puck_pos']
+        pos_goal = o_d['goal_pos']
 
-        if np.linalg.norm(pos_curr[:2] - pos_puck[:2]) > 0.01:
-            return pos_puck + np.array([.0, .0, .1])
-        elif abs(pos_curr[2] - pos_puck[2]) > 0.04:
-            return pos_puck
+        if np.linalg.norm(pos_curr[:2] - pos_puck_[:2]) > 0.01:
+            return pos_puck_ + np.array([.0, .0, .1])
+        elif abs(pos_curr[2] - pos_puck_[2]) > 0.04:
+            return pos_puck_
         elif pos_curr[1] > .7:
             return pos_curr + np.array([.0, -.1, .0])
         elif pos_curr[1] > .6:
             return np.array([.15, .55, pos_curr[2]])
         else:
-            return np.array([pos_curr[0] - .1, .55, pos_curr[2]])
+            return np.array([pos_goal[0] - pos_puck[0] + pos_curr[0], pos_goal[1] - pos_puck[1] + pos_curr[1], pos_curr[2]])
+            # return np.array([pos_curr[0] - .1, .55, pos_curr[2]])
