13372dca0963fa35ce15f159f2b948d4f51302af
DarkDawn233 <hjm0224@qq.com>
2023-04-28 11:05:56 +0800

    update generate data

--- data_generate.py
+++ data_generate.py
@@ -8,9 +8,10 @@ from pathlib import Path
 import h5py
 import threading
 import multiprocessing
+# import cv2
 
-CAMERA_LIST = ["corner3", "corner", "corner2", "topview", "behindGripper"]
-# CAMERA_LIST = ["corner3", "corner", "topview"]
+# CAMERA_LIST = ["corner3", "corner", "corner2", "topview", "behindGripper"]
+CAMERA_LIST = ["corner3", "corner", "topview"]
 
 def show_all_task():
     return TASK_DICK.keys()
@@ -22,7 +23,7 @@ def save_jpeg(image_array, save_path):
 def get_img(env):
     image = {}
     for camera_name in CAMERA_LIST:
-        img = env.render(offscreen=True, camera_name=camera_name)
+        img = env.render(offscreen=True, camera_name=camera_name, resolution=(320, 240))
         image[camera_name] = img
     return image
 
@@ -40,7 +41,7 @@ def show_demo(task_name, seed, demo, gif=False):
         for i, img in enumerate(img_list):
             save_jpeg(img, root_path / (str(i)+".jpeg"))
 
-def run_demo(task_name, seed=0, max_step=400, debug=False):
+def run_demo(task_name, seed=0, max_step=500, debug=False):
     env = TASK_DICK[task_name]['env'](seed=seed)
     policy = TASK_DICK[task_name]['policy']()
 
@@ -118,6 +119,14 @@ def write_h5(task_name, seed, demo):
         else:
             f.create_dataset(k, data=v,compression='gzip')
     f.close()
+
+def exist_h5(task_name, seed):
+    root_path = Path(__file__).parent / 'data' / task_name
+    file_path = root_path / (str(seed) + ".hdf5")
+    if file_path.exists():
+        return True
+    else:
+        return False
     
 def read_h5(task_name, seed):
     file_path = Path(__file__).parent / 'data' / task_name / (str(seed) + ".hdf5")
@@ -163,8 +172,11 @@ def test_env(task_name, thread_num=20):
 def thread_generate_data(t_id, task_name, begin_seed, end_seed):
     root_path = Path(__file__).parent / 'data' / task_name
     root_path.mkdir(exist_ok=True, parents=True)
-    success_file_path = root_path / ("fail_" + str(t_id) + ".txt") 
+    success_file_path = root_path / ("fail_" + str(t_id) + ".txt")
     for seed in range(begin_seed, end_seed):
+        if exist_h5(task_name=task_name, seed=seed):
+            print(f"{task_name} {seed} exist continue.")
+            continue
         demo, success = run_demo(task_name=task_name, seed=seed)
         if not success:
             with open(success_file_path, "a") as f:
@@ -173,11 +185,12 @@ def thread_generate_data(t_id, task_name, begin_seed, end_seed):
             demo = cal_return_to_go(demo)
             write_h5(task_name=task_name, seed=seed, demo=demo)
 
-def generate_data(task_name, thread_num=10):
+def generate_data(task_name, thread_num=10, total_ep=2000):
     thread_list = []
+    each_thread_ep = total_ep // thread_num
     for t_id in range(thread_num):
-        begin_seed = t_id * 200
-        end_seed = (t_id + 1) * 200
+        begin_seed = t_id * each_thread_ep
+        end_seed = (t_id + 1) * each_thread_ep
         # t = threading.Thread(target=thread_generate_data, args=(t_id, task_name, begin_seed, end_seed))
         t = multiprocessing.Process(target=thread_generate_data, args=(t_id, task_name, begin_seed, end_seed))
         t.start()
@@ -185,7 +198,7 @@ def generate_data(task_name, thread_num=10):
     for t in thread_list:
         t.join()
 
-def stat_success(task_name, thread_num=10):
+def stat_success(task_name, thread_num=10, total_ep=2000):
     root_path = Path(__file__).parent / 'data' / task_name
     cnt = 0
     for t_id in range(thread_num):
@@ -193,38 +206,39 @@ def stat_success(task_name, thread_num=10):
         if success_file_path.exists():
             with open(success_file_path, "r") as f:
                 cnt += len(f.readlines())
-    rate = (2000-cnt)/2000
+    rate = (total_ep-cnt)/total_ep
     print("success_rate:", rate) 
     success_rate_path = root_path / "success_rate.txt"
     with open(success_rate_path, "w") as f:
         f.write("success rate: " + str(rate))
 
+def generate_data_main():
+    task_name_list = [
+        "drawer-close-display",
+        "drawer-open-display",
+        # "drawer-place-display",
+        # "drawer-pick-display",
+        ]
+    for task_name in task_name_list:
+        generate_data(task_name=task_name, thread_num=10, total_ep=20000)
+        stat_success(task_name=task_name, thread_num=10, total_ep=20000)
+
 if __name__ == "__main__":
     os.environ['CUDA_VISIBLE_DEVICES']='1'
-    task_name = "drawer-close-display"
-    for seed in range(10):
-        # seed = 6
-        random.seed(seed)
-        np.random.seed(seed)
-        demo, _ = run_demo(task_name=task_name, seed=seed, max_step=400, debug=True)
-        # # demo = cal_return_to_go(demo)
-        show_demo(task_name=task_name, seed=seed, demo=demo, gif=True)
+    generate_data_main()
+    # task_name = "drawer-open-display"
+    # for seed in range(10):
+    #     # seed = 6
+    #     random.seed(seed)
+    #     np.random.seed(seed)
+    #     demo, _ = run_demo(task_name=task_name, seed=seed, max_step=400, debug=True)
+    #     # demo = cal_return_to_go(demo)
+    #     show_demo(task_name=task_name, seed=seed, demo=demo, gif=True)
 
-    # write_h5(task_name=task_name, seed=seed, demo=demo)
+        # write_h5(task_name=task_name, seed=seed, demo=demo)
     # read_h5(task_name=task_name, seed=seed)
 
     # test_env(task_name)
-    # task_name_list = [
-    #     "box-close",
-    #     "button-press-topdown",
-    #     "button-press-topdown-wall",
-    #     "button-press",
-    #     "button-press-wall",
-    #     "coffee-button",
-    #     "coffee-pull"
-    #     ]
-    # for task_name in task_name_list:
-    #     generate_data(task_name=task_name)
-    #     stat_success(task_name=task_name)
+    
 
     
